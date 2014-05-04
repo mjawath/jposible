cca6e6ed8bbc1ccdb37038d2c94100e11f0f8865
mjawathbb <mjawath@gmail.com>
2014-05-04 22:54:04 +0530

    a

---
+++ SM/IS/src/invoicingsystem/NewTask.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package invoicingsystem;
+
+import javax.swing.SwingWorker;
+import org.biz.app.ui.util.CommandTask;
+
+/**
+ *
+ * @author d
+ */
+public class NewTask  extends CommandTask {
+    
+}
