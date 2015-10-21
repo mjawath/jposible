90fd9e0d7e1c8f6e05af1f102e20fc5e27664eee
mjawathbb <mjawath@gmail.com>
2015-10-21 22:22:46 +0530

    mi

---
+++ SM/AS/Components2.0/src/org/biz/app/ui/util/QueryParamListener.java
@@ -0,0 +1,14 @@
+
+package org.biz.app.ui.util;
+
+import java.util.Map;
+
+/**
+ *
+ * @author jawa
+ */
+public interface QueryParamListener {
+
+    public Map<String,Object> getQueryParams();
+    
+}
